02c6feff15b0eeb6aed37441ab9e93839b1d7b30
Moses Lee <moses@fooala.com>
2009-04-30 23:52:10 -0400

    Cleanup views, routes

--- config/environment.rb
+++ config/environment.rb
@@ -34,7 +34,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "ruby-openid", :lib => 'openid'
-  config.gem "oauth"
+  config.gem "oauth", :version => '0.3.0'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
